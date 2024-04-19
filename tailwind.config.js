c8af730ffe4f32f0ebb610f4fea52140b6057c94
chihuy124 <22520577@gm.uit.edu.vn>
2024-04-19 11:51:52 +0700

    huy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'sans': ['museo-slab-500'],
+        'museo-slab-100': ['museo-slab-100'],
+        'inter-400': ['inter-400'],
+        'inter-700': ['inter-700'],
+        'aubrey': ['aubrey'],
+      },
+
+      keyframes: {
+        slidein:{
+          from:{
+            opacity: "0",
+            transform: "translateY(10px)",
+          },
+          to:{
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        
+      },
+      animation: {
+        slidein: "slidein 1s ease-in-out",
+      },
       colors: {
         'opacity-cde1df': '#CDE1DF', 
         'opacity-00b5ac': '#00B5AC',
