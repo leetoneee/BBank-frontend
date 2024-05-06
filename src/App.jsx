a9f378616fc96930e84e432470049b3378dbcec0
chihuy124 <22520577@gm.uit.edu.vn>
2024-05-06 10:26:13 +0700

    edit Login and add FastFeatures

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,16 @@ function App(props) {
   return (
     <Routes>
       <Route path='login' element={<Login />} />
-      <Route path=':userId/*' element={<RequireAuth><MainLayout /></RequireAuth>}>
+      <Route path=':userId/*' element={<MainLayout />}>
         <Route path='home' element={<Home />} />
         <Route exact path='utilities' element={<Home />} />
         <Route exact path='setting' element={<Home />} />
-        <Route exact path='setting/fastfeatures' element={<FastFeatures/>} />
+        <Route exact path='setting/fastfeatures' element={<FastFeatures />} />
         <Route exact path='contact' element={<Home />} />
         <Route exact path='home/transfer' element={<Transfer />} />
       </Route>
     </Routes>
-     
+
   )
 }
 
