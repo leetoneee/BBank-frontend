f4f4dac593b3ea6013a60b4cef8fb7a047ba6bd1
chihuy124 <22520577@gm.uit.edu.vn>
2024-05-09 21:01:04 +0700

    print BienLai

--- src/components/TransferStepper/steps/Result.jsx
+++ src/components/TransferStepper/steps/Result.jsx
@@ -1,14 +1,33 @@
 import { classNames } from "../../classNames/classNames";
 import { useNavigate } from "react-router-dom";
 import { FaCheckCircle } from "react-icons/fa";
-import logo from '../../../assets/icons/logo.svg'
+import logo from '../../../assets/icons/logoBBank.png'
 import { useDispatch, useSelector } from "react-redux";
 import formatToVND from "../../../utils/formatToVND";
 import { formatDateResult } from "../../../utils/formatDateAndTime";
 import { reset as resetTransfer } from "../../../redux/customer/transfer/transferSlice";
 import { reset as resetCheckAccount } from "../../../redux/system/checkAccountExist/checkExistSlice";
+import React, { useRef } from "react";
+import { useReactToPrint } from 'react-to-print';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
-function Result(props) {
+export function Result(props) {
+    const getPageMargins = (marginTop, marginRight, marginBottom, marginLeft) => {
+        return `
+          @page {
+            margin: ${marginTop} ${marginRight} ${marginBottom} ${marginLeft} !important;
+            size: A4;
+          }
+        `;
+    };
+
+    // Define your desired margin values
+    const marginTop = '0';
+    const marginBottom = '0';
+    const marginRight = '0.69in'; // Assuming inches as the unit
+    const marginLeft = '0.69in'; // Assuming inches as the unit
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -27,8 +46,16 @@ function Result(props) {
         props.handleInitNewTransaction();
     }
 
+    const contentToPrint = useRef(null);
+    const handlePrint = useReactToPrint({
+        documentTitle: "Print This Document",
+        onBeforePrint: () => console.log("before printing..."),
+        onAfterPrint: () => console.log("after printing..."),
+        removeAfterPrint: true,
+    });
+
     return (
-        <div className=" container flex flex-col gap-[50px] mt-4 mb-8 ">
+        <div ref={contentToPrint} className=" container flex flex-col gap-[50px] mt-4 mb-8 ">
             <div className="w-full flex flex-col bg-[#26383C] rounded-[10px] py-10 px-10 gap-4 shadow-green-400 shadow-sm">
                 <div className="flex justify-center items-center">
                     <div className="flex items-center">
@@ -46,8 +73,8 @@ function Result(props) {
             </div>
 
             {/* Thông tin chuyển khoản */}
-            <div className="w-full bg-[#26383C] rounded-[10px] py-10 px-10">
-                <div className="flex flex-col gap-8">
+            <div className=" w-full bg-[#26383C] rounded-[10px] py-10 px-10">
+                <div className=" flex flex-col gap-8">
                     <div className="grid grid-cols-3 grid-rows-1 gap-8">
                         <span className="col-start-1 text-[#A5ACAE] text-xl  self-center ">
                             Tên người thụ hưởng
@@ -99,7 +126,7 @@ function Result(props) {
                     Quay về
                 </button>
                 {/* Print */}
-                <button
+                <button onClick={() => { handlePrint(null, () => contentToPrint.current); }}
                     className={classNames(" text-2xl bg-[#475255]/[90%] text-white py-2 w-64 rounded-[10px] font-bold cursor-pointer hover:bg-[#475255]/[60%] transition duration-200 ease-in-out ")}>
                     In biên lai
                 </button>
@@ -110,8 +137,7 @@ function Result(props) {
                 className=" mt-3 text-2xl self-center  bg-gradient-to-r from-[#57B122] to-[#09812E] hover:from-[#09812E] hover:to-[#57B122] text-white py-2 w-96   rounded-[15px] font-bold cursor-pointer hover:bg-[#475255]/[60%] transition duration-200 ease-in-out">
                 Thực hiện giao dịch mới
             </button>
+            <style>{getPageMargins(marginTop, marginRight, marginBottom, marginLeft)}</style>
         </div >
     )
 }
-
-export default Result;
