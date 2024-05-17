d9f520d3a0e518a34461feae5d70b38f9c491157
lehuudoUIT <22520254@gm.uit.edu.vn>
2024-05-17 10:14:05 +0700

    create toast message

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { Bounce, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <Router>
         <App />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
+        {/* Same as */}
+        <ToastContainer />
       </Router>
     </React.StrictMode>
-  </Provider>,
-)
+  </Provider>
+);
